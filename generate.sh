7fb6efccd15a43435ac8439889d593457d35df15
jujimeizuo <3058852685@qq.com>
2023-04-05 13:11:04 +0000

    [feat]: revert

--- generate.sh
+++ generate.sh
@@ -19,10 +19,10 @@ namespace=$2
 
 command_error_exit mkdir $project_name
 command_error_exit cd $project_name
-command_error_exit git clone https://github.com/jujimeizuo-yin/jujimeizuo.git
-command_error_exit cp jujimeizuo/Makefile .
-command_error_exit cp -rf jujimeizuo/template/* .
-command_error_exit cp -rf jujimeizuo/template/* .
+command_error_exit git clone git@github.com:JUHPS/juhps-server.git
+command_error_exit cp sylar/Makefile .
+command_error_exit cp -rf sylar/template/* .
+command_error_exit cp -rf sylar/template/* .
 command_error_exit mv template ${namespace}
 command_error_exit sed -i "s/project_name/${project_name}/g" CMakeLists.txt
 command_error_exit sed -i "s/template/${namespace}/g" CMakeLists.txt
